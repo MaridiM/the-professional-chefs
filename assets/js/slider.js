f7ef0bb33de508a75682eacc720b3c31a498c151
MaridiM <maridim92@gmail.com>
2022-06-01 12:53:21 +0300

    Init Commit

---
+++ assets/js/slider.js
@@ -0,0 +1,21 @@
+// const picturePath = (picName) => {
+//     return `url("./assets/img/gallery/${picName}.jpg")`
+// }
+// const pictureNames = [
+//     1, 2, 3, 4, 5, 6, 7, 8,
+// ]
+
+// const app = document.querySelector('.app')
+
+
+// let i = 0
+// setInterval(() => {
+//     if(i > pictureNames.length -1) {
+//         i = 0
+//     }
+//     console.log(i)
+//     app.style.backgroundImage = picturePath(pictureNames[i])
+//     i++
+
+
+// }, 2000)
---
+++ sass-watch.bat
@@ -0,0 +1,4 @@
+start /min sass --watch assets/styles/sass:assets/styles/css --no-source-map --style expanded
+@REM style type
+@REM expanded 
+@REM compressed
